f8245b6d89940bada91707dd68e97a5c69445cf7
Duyen Nguyen <dnng1410@gmail.com>
2024-12-15 15:21:53 +0700

    implement nestjs config to load env vars to modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,8 +6,18 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, UserModule, BookmarkModule, PrismaModule],
+  imports: [
+    // load .env file into the project
+    ConfigModule.forRoot({
+      isGlobal: true, // make the config module available to all other modules
+    }),
+    AuthModule,
+    UserModule,
+    BookmarkModule,
+    PrismaModule,
+  ],
 })
 export class AppModule {}
